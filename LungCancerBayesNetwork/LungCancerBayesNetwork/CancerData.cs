b2b7f1196abf1adb1e5a2ec8147dc20d5ef628ce
Adrian Tomczyk <200791@student.pwr.edu.pl>
2016-12-09 19:05:56 +0100

    Change project language!

---
+++ LungCancerBayesNetwork/LungCancerBayesNetwork/CancerData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LungCancerBayesNetwork
+{
+    class CancerData
+    {
+        public CancerData(Int16 cancerClass, Int16 [] attributes)
+        {
+            this.cancerClass = cancerClass;
+            this.attributes = attributes;
+        }
+        private Int16 cancerClass { get; }
+        private Int16 [] attributes { get; }
+        public String ToString()
+        {
+            String result = "";
+            result += "Class: " + cancerClass.ToString()+"\n";
+            result += "Attributes: " + String.Join(",",attributes);
+            return result;
+        }
+    }
+}
---
+++ LungCancerBayesNetwork/LungCancerBayesNetwork/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LungCancerBayesNetwork
+{
+    class Program
+    {
+        static List<String> getFileData(String path)
+        {
+            List<String> data = new List<String>();
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    do
+                    {
+                        data.Add(reader.ReadLine());
+                    } while (!reader.EndOfStream);
+                    return data;
+                }
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine("FileNotFound");
+                System.Console.WriteLine(e.Message);
+                return data;
+            }
+
+        }
+        static List<CancerData> getDataFromFile(String path)
+        {
+            List<CancerData> data = new List<CancerData>();
+            List<String> fileData = getFileData(path);
+            foreach(String s in fileData)
+            {
+                if (s.Length > 0)
+                {
+                    String[] values = s.Split(',');
+                    Int16 cancerClass = Int16.Parse(values[0]);
+                    Int16[] attributes = new Int16[values.Length - 1];
+                    for (int i = 1; i < values.Length; ++i)
+                    {
+                        try
+                        {
+                            attributes[i - 1] = Int16.Parse(values[i]);
+                        }
+                        catch (Exception e)
+                        {
+                            attributes[i - 1] = -1;//Obsługa znaków zapytania w pliku
+                        }
+
+                    }
+                    data.Add(new CancerData(cancerClass, attributes));
+                }
+
+            }
+            return data;
+        }
+        static void Main(string[] args)
+        {
+            System.Console.Write(getDataFromFile("lung-cancer.data.txt").ElementAt(20).ToString());
+            System.Console.ReadKey();
+        }
+    }
+}
