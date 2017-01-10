c5352521669445dde8d8cf889142fe52a87c9dfb
Adrian Tomczyk <200791@student.pwr.edu.pl>
2017-01-10 10:29:20 +0100

    Fixed probability counting, and arc definitions

--- LungCancerBayesNetwork/LungCancerBayesNetwork/CancerData.cs
+++ LungCancerBayesNetwork/LungCancerBayesNetwork/CancerData.cs
@@ -19,20 +19,26 @@ namespace LungCancerBayesNetwork
 
         public static double [] countProbabilityDistributionForChildVertex(List<CancerData> data, Int32 elementIndex, Int32 [] childAttributesIndexes)
         {
-            double[] result = new double[4 * 4 * childAttributesIndexes.Length];
+            double[] result = new double[(int)Math.Pow(4,childAttributesIndexes.Length+1)];
             foreach(CancerData element in data)
             {
-                Int32 attrIndex = 0;
-                foreach (Int32 index in childAttributesIndexes)
-                {
-                    Int32 elementValue = element.attributes[elementIndex-1];
-                    Int32 attributeValue = element.attributes[index-1];
-                    if (elementValue>=0 && attributeValue>= 0)
-                    {
-                        result[(attrIndex*16)+(attributeValue*4)+elementValue]++;
-                    }
-                    attrIndex++;
-                }
+                //Int32 attrIndex = 0;
+                //foreach (Int32 index in childAttributesIndexes)
+                //{
+                //    Int32 elementValue = element.attributes[elementIndex-1];
+                //    Int32 attributeValue = element.attributes[index-1];
+                //    if (elementValue>=0 && attributeValue>= 0)
+                //    {
+                //        result[]++;
+                //        result[(attrIndex * 16) + (attributeValue * 4) + elementValue]++;
+                //    }
+                //    attrIndex++;
+                //}
+                int x = element.attributes[elementIndex-1] * 64;
+                int y = element.attributes[childAttributesIndexes[0]-1] * 16;
+                int z = element.attributes[childAttributesIndexes[1]-1] * 4; 
+                int t = element.attributes[childAttributesIndexes[2]-1];
+                if(x>0 && y>0 && z>0 && t>0)result[x+y+z+t]++;
             }
             for(int i = 0; i < result.Length; i++)
             {
@@ -43,21 +49,27 @@ namespace LungCancerBayesNetwork
 
         public static double[] countProbabilityDistributionForResult(List<CancerData> data, Int32[] childAttributesIndexes)
         {
-            double[] result = new double[3 * 4 * childAttributesIndexes.Length];
+            double[] result = new double[3 * (int)Math.Pow(4,childAttributesIndexes.Length)];
             foreach (CancerData element in data)
             {
-                Int32 attrIndex = 0;
-                foreach (Int32 index in childAttributesIndexes)
-                {
-                    Int32 elementValue = element.cancerClass;
-                    Int32 attributeValue = element.attributes[index-1];
-                    if (elementValue >= 0 && attributeValue >= 0)
-                    {
-                        result[(attrIndex * 12) + (attributeValue * 4) + elementValue]++;
-                    }
-                    attrIndex++;
-                }
+                int x = (element.cancerClass-1) * 48;
+                int y = element.attributes[childAttributesIndexes[0]-1] * 16;
+                int z = element.attributes[childAttributesIndexes[1]-1] * 4;
+                int t = element.attributes[childAttributesIndexes[2]-1];
+                if(x>0 && y>0 && z>0 && t>0) result[x + y + z + t]++;
+                //    Int32 attrIndex = 0;
+                //    foreach (Int32 index in childAttributesIndexes)
+                //    {
+                //        Int32 elementValue = element.cancerClass;
+                //        Int32 attributeValue = element.attributes[index-1];
+                //        if (elementValue >= 0 && attributeValue >= 0)
+                //        {
+                //            result[(attrIndex * 12) + (attributeValue * 4) + elementValue]++;
+                //        }
+                //        attrIndex++;
+                //    }
             }
+
             for (int i = 0; i < result.Length; i++)
             {
                 result[i] /= data.Count;
--- LungCancerBayesNetwork/LungCancerBayesNetwork/LungCancerBayes.cs
+++ LungCancerBayesNetwork/LungCancerBayesNetwork/LungCancerBayes.cs
@@ -44,107 +44,106 @@ namespace LungCancerBayesNetwork
             double[] parentProbability;
 
             //Left
-            parentProbability = CancerData.countProbabilityForParentVertex(Tdata, 3);
+            parentProbability = CancerData.countProbabilityForParentVertex(Ldata, 3);
             Net.AddNode(Network.NodeType.Cpt, "p3");
             SetNodeStates("p3");
             Net.SetNodeDefinition("p3", parentProbability);
 
-            parentProbability = CancerData.countProbabilityForParentVertex(Tdata, 26);
+            parentProbability = CancerData.countProbabilityForParentVertex(Ldata, 26);
             Net.AddNode(Network.NodeType.Cpt, "p26");
             SetNodeStates("p26");
             Net.SetNodeDefinition("p26", parentProbability);
 
-            parentProbability = CancerData.countProbabilityForParentVertex(Tdata, 38);
+            parentProbability = CancerData.countProbabilityForParentVertex(Ldata, 38);
             Net.AddNode(Network.NodeType.Cpt, "p38");
             SetNodeStates("p38");
             Net.SetNodeDefinition("p38", parentProbability);
 
             //Middle
-            parentProbability = CancerData.countProbabilityForParentVertex(Tdata, 13);
+            parentProbability = CancerData.countProbabilityForParentVertex(Ldata, 13);
             Net.AddNode(Network.NodeType.Cpt, "p13");
             SetNodeStates("p13");
             Net.SetNodeDefinition("p13", parentProbability);
 
-            parentProbability = CancerData.countProbabilityForParentVertex(Tdata, 29);
+            parentProbability = CancerData.countProbabilityForParentVertex(Ldata, 29);
             Net.AddNode(Network.NodeType.Cpt, "p29");
             SetNodeStates("p29");
             Net.SetNodeDefinition("p29", parentProbability);
 
-            parentProbability = CancerData.countProbabilityForParentVertex(Tdata, 41);
+            parentProbability = CancerData.countProbabilityForParentVertex(Ldata, 41);
             Net.AddNode(Network.NodeType.Cpt, "p41");
             SetNodeStates("p41");
             Net.SetNodeDefinition("p41", parentProbability);
 
             //Right
-            parentProbability = CancerData.countProbabilityForParentVertex(Tdata, 15);
+            parentProbability = CancerData.countProbabilityForParentVertex(Ldata, 15);
             Net.AddNode(Network.NodeType.Cpt, "p15");
             SetNodeStates("p15");
             Net.SetNodeDefinition("p15", parentProbability);
 
-            parentProbability = CancerData.countProbabilityForParentVertex(Tdata, 37);
+            parentProbability = CancerData.countProbabilityForParentVertex(Ldata, 37);
             Net.AddNode(Network.NodeType.Cpt, "p37");
             SetNodeStates("p37");
             Net.SetNodeDefinition("p37", parentProbability);
 
-            parentProbability = CancerData.countProbabilityForParentVertex(Tdata, 42);
+            parentProbability = CancerData.countProbabilityForParentVertex(Ldata, 42);
             Net.AddNode(Network.NodeType.Cpt, "p42");
             SetNodeStates("p42");
             Net.SetNodeDefinition("p42", parentProbability);
 
             //Left
-            childProbability = CancerData.countProbabilityDistributionForChildVertex(Tdata, 6, SetIndexes(3, 26, 38));
+            childProbability = CancerData.countProbabilityDistributionForChildVertex(Ldata, 6, SetIndexes(3, 26, 38));
             Net.AddNode(Network.NodeType.Cpt, "c6");
-            SetNodeStates("c6");
-            Net.SetNodeDefinition("c6", parentProbability);
 
-            childProbability = CancerData.countProbabilityDistributionForChildVertex(Tdata, 2, SetIndexes(3, 26, 38));
-            Net.AddNode(Network.NodeType.Cpt, "c2");
-            SetNodeStates("c2");
-            Net.SetNodeDefinition("c2", parentProbability);
-
-            //Middle
-            childProbability = CancerData.countProbabilityDistributionForChildVertex(Tdata, 20, SetIndexes(13, 29, 41));
-            Net.AddNode(Network.NodeType.Cpt, "c20");
-            SetNodeStates("c20");
-            Net.SetNodeDefinition("c20", parentProbability);
-
-            childProbability = CancerData.countProbabilityDistributionForChildVertex(Tdata, 56, SetIndexes(13, 29, 41));
-            Net.AddNode(Network.NodeType.Cpt, "c56");
-            SetNodeStates("c56");
-            Net.SetNodeDefinition("c56", parentProbability);
-
-            //Right
-            childProbability = CancerData.countProbabilityDistributionForChildVertex(Tdata, 19, SetIndexes(15, 37, 42));
-            Net.AddNode(Network.NodeType.Cpt, "c19");
-            SetNodeStates("c19");
-            Net.SetNodeDefinition("c19", parentProbability);
-
-            //Left
             Net.AddArc("p3", "c6");
-            Net.AddArc("p3", "c2");
-
             Net.AddArc("p26", "c6");
-            Net.AddArc("p26", "c2");
-
             Net.AddArc("p38", "c6");
+            
+            SetNodeStates("c6");
+            Net.SetNodeDefinition("c6", childProbability);
+
+            childProbability = CancerData.countProbabilityDistributionForChildVertex(Ldata, 2, SetIndexes(3, 26, 38));
+            Net.AddNode(Network.NodeType.Cpt, "c2");
+
+            Net.AddArc("p3", "c2");
+            Net.AddArc("p26", "c2");
             Net.AddArc("p38", "c2");
 
+            SetNodeStates("c2");
+            Net.SetNodeDefinition("c2", childProbability);
+
             //Middle
+            childProbability = CancerData.countProbabilityDistributionForChildVertex(Ldata, 20, SetIndexes(13, 29, 41));
+            Net.AddNode(Network.NodeType.Cpt, "c20");
+
             Net.AddArc("p13", "c20");
-            Net.AddArc("p13", "c56");
-
             Net.AddArc("p29", "c20");
-            Net.AddArc("p29", "c56");
-
             Net.AddArc("p41", "c20");
+
+            SetNodeStates("c20");
+            Net.SetNodeDefinition("c20", childProbability);
+
+            childProbability = CancerData.countProbabilityDistributionForChildVertex(Ldata, 56, SetIndexes(13, 29, 41));
+            Net.AddNode(Network.NodeType.Cpt, "c56");
+
+            Net.AddArc("p13", "c56");
+            Net.AddArc("p29", "c56");
             Net.AddArc("p41", "c56");
 
-            //Right
-            Net.AddArc("p15", "c19");
+            SetNodeStates("c56");
+            Net.SetNodeDefinition("c56", childProbability);
 
+            //Right
+            childProbability = CancerData.countProbabilityDistributionForChildVertex(Ldata, 19, SetIndexes(15, 37, 42));
+            Net.AddNode(Network.NodeType.Cpt, "c19");
+
+            Net.AddArc("p15", "c19");
             Net.AddArc("p37", "c19");
-            
             Net.AddArc("p42", "c19");
+
+            SetNodeStates("c19");
+            Net.SetNodeDefinition("c19", childProbability);
+
 
             Net.WriteFile("lungcancer.xdsl");
             return null;
